d3548eb33e03afc06162cfb55ccd473774578cec
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-10 11:06:41 +0000

    fix: remove unused auth form styles

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,4 +1,3 @@
-import { styled } from '@mui/material/styles';
 import { CardContent } from '@mui/material';
 import { Form } from 'react-admin';
 import { FC, ReactNode } from 'react';
@@ -10,44 +9,18 @@ type AuthFormProps = {
   className?: string;
 };
 
-const PREFIX = 'RaAuthForm';
-
-const LoginFormClasses = {
-  content: `${PREFIX}-content`,
-  button: `${PREFIX}-button`,
-  icon: `${PREFIX}-icon`,
-  link: `${PREFIX}-link`,
-};
-
-const StyledForm = styled(Form, {
-  name: PREFIX,
-  overridesResolver: (_, styles) => styles.root,
-})(({ theme }) => ({
-  [`& .${LoginFormClasses.content}`]: {
-    width: 400,
-  },
-  [`& .${LoginFormClasses.button}`]: {
-    marginTop: theme.spacing(2),
-  },
-  [`& .${LoginFormClasses.link}`]: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginTop: theme.spacing(2),
-  },
-}));
-
 export const AuthForm: FC<AuthFormProps> = ({
   children,
   className,
   onSubmit,
   validate,
 }: AuthFormProps) => (
-  <StyledForm
+  <Form
     onSubmit={onSubmit}
     mode="onChange"
     validate={validate}
     noValidate={!validate}
     className={className}>
-    <CardContent className={LoginFormClasses.content}>{children}</CardContent>
-  </StyledForm>
+    <CardContent sx={{ width: '400px' }}>{children}</CardContent>
+  </Form>
 );

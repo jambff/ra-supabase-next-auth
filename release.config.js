8029afeadd69197a38f661c53c803356c0add390
Alexander Mendes <alexanderhmendes@gmail.com>
2023-01-11 21:06:09 +0000

    fix: update release config

--- release.config.js
+++ release.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   branches: [{ name: 'main' }],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/npm',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+    '@semantic-release/git',
+  ],
 };

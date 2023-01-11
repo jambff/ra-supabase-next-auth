d094328a3b23ba487cd0c929f9dd6ac791a535cd
Alex Mendes <alexanderhmendes@gmail.com>
2023-01-11 21:14:05 +0000

    fix: update release config

--- release.config.js
+++ release.config.js
@@ -5,6 +5,14 @@ module.exports = {
     '@semantic-release/npm',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
-    '@semantic-release/git',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
